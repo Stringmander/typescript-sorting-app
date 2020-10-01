45cd646aefa1c08768dad5190b1c033bee7dc895
Stringmander <nrusso@protonmail.com>
2020-10-01 14:17:57 -0400

    Create sorter scaffolding

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-console.log('hello world');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const logSomething = () => {
-  console.log('something');
-};
-logSomething();
+  sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
