51767630c45b62403ca5301bc04711a993ef52b2
Stringmander <nrusso@protonmail.com>
2020-10-05 12:02:47 -0400

    Create LinkedLIst.ts enabling linked list sorting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharctersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const numberSorter = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollection('Xaayb');
 const stringSorter = new Sorter(charactersCollection);
 stringSorter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const listSorter = new Sorter(linkedList);
+listSorter.sort();
+linkedList.print();
